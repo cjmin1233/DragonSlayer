76c6d909f922d63e70345d196082c50266604d08
junVelope <atimie12@g.skku.edu>
2023-08-07 15:43:37 +0900

    해상도 변경 옵션 추가

---
+++ Assets/Scripts/VideoOption.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoOption : MonoBehaviour
+{
+    FullScreenMode screenMode;
+    public TMP_Dropdown resolutionDropdown;
+    public Toggle fullscreenBtn;
+    List<Resolution> resolutions = new List<Resolution>();
+    int resolutionNum;
+
+    void Start()
+    {
+        InitUI();
+    }
+
+    private void InitUI()
+    {
+        for(int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            if (Screen.resolutions[i].refreshRate == 60)
+                resolutions.Add(Screen.resolutions[i]);
+        }
+
+        resolutionDropdown.options.Clear();
+
+        int optionNum = 0;
+        foreach (Resolution item in resolutions)
+        {
+            TMP_Dropdown.OptionData option = new TMP_Dropdown.OptionData();
+            option.text = item.width + " x " + item.height + " " + item.refreshRateRatio + "hz";
+            resolutionDropdown.options.Add(option);
+
+            if (item.width == Screen.width && item.height == Screen.height)
+                resolutionDropdown.value = optionNum;
+            optionNum++;
+        }
+        resolutionDropdown.RefreshShownValue();
+
+        fullscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow)?true:false;
+    }
+
+    public void DropboxOptionChange(int x)
+    {
+        resolutionNum = x;
+    }
+
+    public void FullScreenBtn(bool isFull)
+    {
+        screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+    }
+
+    public void OkBtnClick()
+    {
+        Screen.SetResolution(resolutions[resolutionNum].width,
+            resolutions[resolutionNum].height,
+            screenMode);
+    }
+}
