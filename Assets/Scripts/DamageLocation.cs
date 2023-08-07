e7f95a1f15411f8bacd002d59d83d8dbfc64b9ef
junVelope <atimie12@g.skku.edu>
2023-08-07 10:20:47 +0900

    데미지 텍스트/창모드 구현

---
+++ Assets/Scripts/DamageLocation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageLocation : MonoBehaviour
+{
+    public GameObject hudDamageText;
+    public Transform hudPos;
+
+    ////public override void TakeDamage(int damage) //데미지입는 함수명으로 변경
+    //{
+    //    GameObject hudText = Instantiate(hudDamageText); //생성할 텍스트 오브젝트
+    //    hudText.transform.position = hudPos.position; //표시할 위치
+    //    hudText.GetComponent<DamageText>().damage = damage;
+    //    base.TakeDamage(damage);
+    //}
+}
