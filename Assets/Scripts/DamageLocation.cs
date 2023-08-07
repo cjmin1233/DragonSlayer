f87ceab06b7373589afe886f65099c50e93919ee
junVelope <atimie12@g.skku.edu>
2023-08-07 14:04:27 +0900

    데미지텍스트 구현완료

--- Assets/Scripts/DamageLocation.cs
+++ Assets/Scripts/DamageLocation.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageLocation : MonoBehaviour
-{
-    public GameObject hudDamageText;
-    public Transform hudPos;
+//public class DamageLocation : MonoBehaviour
+//{
+//    public GameObject hudDamageText;
+//    public Transform hudPos;
 
-    ////public override void TakeDamage(int damage) //데미지입는 함수명으로 변경
-    //{
-    //    GameObject hudText = Instantiate(hudDamageText); //생성할 텍스트 오브젝트
-    //    hudText.transform.position = hudPos.position; //표시할 위치
-    //    hudText.GetComponent<DamageText>().damage = damage;
-    //    base.TakeDamage(damage);
-    //}
-}
+//    public override void TakeDamage(int damage) //데미지입는 함수명으로 변경
+//    {
+//        GameObject hudText = Instantiate(hudDamageText); //생성할 텍스트 오브젝트
+//        hudText.transform.position = hudPos.position; //표시할 위치
+//        hudText.GetComponent<DamageText>().damage = damage;
+//        base.TakeDamage(damage);
+//    }
+//}
--- Assets/Scripts/DamageText.cs
+++ Assets/Scripts/DamageText.cs
@@ -1,16 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
 
 public class DamageText : MonoBehaviour
 {
     private float moveSpeed;
     private float alphaSpeed;
     private float destroyTime;
-    TextMeshPro text;
-    Color alpha;
-    public int damage;
+    private TextMeshPro text; // TextMeshProUGUI 컴포넌트 사용
+    private Color alpha;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshPro>();
+        alpha = text.color;
+    }
 
     private void Start()
     {
@@ -18,18 +23,22 @@ public class DamageText : MonoBehaviour
         alphaSpeed = 2.0f;
         destroyTime = 2.0f;
 
-        text = GetComponent<TextMeshPro>();
-        alpha = text.color;
-        text.text = damage.ToString();
         Invoke("DestroyObject", destroyTime);
     }
 
     private void Update()
     {
-        transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0)); //텍스트 위치
+        // 텍스트를 위로 이동
+        transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
 
-        alpha.a = Mathf.Lerp(alpha.a, 0, Time.deltaTime * alphaSpeed); // 텍스트 알파값
+        // 텍스트 알파값 조절
+        alpha.a = Mathf.Lerp(alpha.a, 0, Time.deltaTime * alphaSpeed);
         text.color = alpha;
+    }
+
+    public void ShowDamage(int damage)
+    {
+        text.text = damage.ToString(); // 받은 데미지 값을 텍스트로 설정
     }
 
     private void DestroyObject()
