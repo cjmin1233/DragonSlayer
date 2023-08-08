bb6c50a263e374f39c5e798fb5556d6d05015813
junVelope <atimie12@g.skku.edu>
2023-08-08 10:52:23 +0900

    해상도 16:9 비율만 가져오기 & 빌드에러 수정
    드랍다운 옵션에 추가하는 부분의 조건문 변경
    빌드시 나오는 애니메이션 스크립트 오류 주석처리

--- Assets/Scripts/VideoOption.cs
+++ Assets/Scripts/VideoOption.cs
@@ -6,11 +6,24 @@ using UnityEngine.UI;
 
 public class VideoOption : MonoBehaviour
 {
+
     FullScreenMode screenMode;
     public TMP_Dropdown resolutionDropdown;
     public Toggle fullscreenBtn;
     List<Resolution> resolutions = new List<Resolution>();
     int resolutionNum;
+
+    static int GCD(int num1, int num2)
+    {
+        int remainder;
+        while (num2 != 0)
+        {
+            remainder = num1 % num2;
+            num1 = num2;
+            num2 = remainder;
+        }
+        return num1;
+    }
 
     void Start()
     {
@@ -19,10 +32,13 @@ public class VideoOption : MonoBehaviour
 
     private void InitUI()
     {
-        for(int i = 0; i < Screen.resolutions.Length; i++)
+        resolutions.Clear(); // 기존의 resolutions 리스트를 초기화
+
+        for (int i = 0; i < Screen.resolutions.Length; i++)
         {
-            // if (Screen.resolutions[i].refreshRate == 60)
-            resolutions.Add(Screen.resolutions[i]);
+            int MAX = GCD(Screen.resolutions[i].width, Screen.resolutions[i].height);
+            if ((Screen.resolutions[i].width / MAX == 16) && (Screen.resolutions[i].height / MAX == 9))
+                resolutions.Add(Screen.resolutions[i]);
         }
 
         resolutionDropdown.options.Clear();
@@ -40,7 +56,7 @@ public class VideoOption : MonoBehaviour
         }
         resolutionDropdown.RefreshShownValue();
 
-        fullscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow)?true:false;
+        fullscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
     }
 
     public void DropboxOptionChange(int x)
