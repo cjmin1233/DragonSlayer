8cd0b9a5cd1d049e1bd3a75ccf0746fb26ccb887
cjmin1233 <cjmin1233@gmail.com>
2023-08-17 22:47:33 +0900

    DamageText 수정
    EffectManager에 DamageText 추가
    Weapon 공격시 데미지 출력

--- Assets/Scripts/DamageTextTest.cs
+++ Assets/Scripts/DamageTextTest.cs
@@ -1,21 +1,21 @@
-using UnityEngine;
-using UnityEngine.EventSystems;
-
-public class DamageTextTest : MonoBehaviour
-{
-    public int damage = 1111;
-    public GameObject hudDamageText;
-    public Transform hudPos;
-
-    public void TakenDamage()
-    {
-        Debug.Log("데미지를 받았습니다.");
-        GameObject hudText = Instantiate(hudDamageText);
-        hudText.transform.position = hudPos.position;
-        DamageText damageText = hudText.GetComponent<DamageText>();
-        if (damageText != null)
-        {
-            damageText.ShowDamage(damage); // DamageText 스크립트의 ShowDamage 호출
-        }
-    }
-}
+// using UnityEngine;
+// using UnityEngine.EventSystems;
+//
+// public class DamageTextTest : MonoBehaviour
+// {
+//     public int damage = 1111;
+//     public GameObject hudDamageText;
+//     public Transform hudPos;
+//
+//     public void TakenDamage()
+//     {
+//         Debug.Log("데미지를 받았습니다.");
+//         GameObject hudText = Instantiate(hudDamageText);
+//         hudText.transform.position = hudPos.position;
+//         DamageText damageText = hudText.GetComponent<DamageText>();
+//         if (damageText != null)
+//         {
+//             damageText.ShowDamage(damage); // DamageText 스크립트의 ShowDamage 호출
+//         }
+//     }
+// }
