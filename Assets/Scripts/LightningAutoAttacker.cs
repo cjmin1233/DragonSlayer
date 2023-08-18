2dbddaecf523853c12c3a577b8af2d0599f5e6cb
cjmin1233 <cjmin1233@gmail.com>
2023-08-18 10:39:48 +0900

    Beholder Projectile 이펙트 수정
    LightningAutoAttacker 파티클 씹힘 수정

--- Assets/Scripts/LightningAutoAttacker.cs
+++ Assets/Scripts/LightningAutoAttacker.cs
@@ -9,9 +9,11 @@ public class LightningAutoAttacker : MonoBehaviour
     [SerializeField] private float attackDuration;
     [SerializeField] private GameObject attackObject;
 
+    private ParticleSystem _particleSystem;
     private Coroutine autoAttackRoutine;
     private void OnEnable()
     {
+        _particleSystem ??= attackObject.GetComponent<ParticleSystem>();
         if (autoAttackRoutine is not null) StopCoroutine(autoAttackRoutine);
         autoAttackRoutine = StartCoroutine(AutoAttackRoutine());
     }
@@ -22,6 +24,7 @@ public class LightningAutoAttacker : MonoBehaviour
         {
             attackObject.transform.position = MyUtility.GetRandomPointBet2Circles(transform.position, 0f, attackRadius);
             attackObject.SetActive(true);
+            if(_particleSystem is not null) _particleSystem.Play();
             yield return new WaitForSeconds(attackDuration);
         }
     }
